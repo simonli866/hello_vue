c382a51078eb378a5c599b2819ace86ef78ce6d7
simon Li <13132549866@163.com>
2024-05-10 22:51:02 +0800

    vue2 grammar

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,23 @@
         <!-- html -->
         <div class="app">
                 <h1>你好啊</h1>
+                <Person />
 
         </div>
 </template>
 
 <script lang="ts">
+import Person from './components/Person.vue'
+
 // JS 或TS
 export default {
         // 组件名
-                name:"App"
+        name: "App",
+        components: {
+                // 注册组件
+                Person 
         }
+}
 </script>
 
 <style>
---
+++ src/components/Person.vue
@@ -0,0 +1,56 @@
+<template>
+        <!-- html -->
+        <div class="person">
+                <h2>姓名：{{name}}</h2>
+                <h2>年龄：{{age}}</h2>
+                <button @click="changeName">修改名字</button>
+                <button @click="changeAge">修改年龄</button>
+                <button @click="showTel">查看联系方式</button>
+        </div>
+</template>
+
+<script lang="ts">
+// JS 或TS
+export default {
+        // 组件名
+        name: "Person",
+        data() {
+                return {
+                        name: "zhangsan",
+                        age: 18,
+                        tel: "xxxxxxx"
+                }
+        },
+        methods: {
+                changeName() {
+                        this.name = "lisi"
+                },
+                changeAge() {
+                        this.age +=1
+                },
+                showTel() {
+                        alert(this.tel)
+                }
+        }
+}
+</script>
+
+<style scoped>
+        /* css */
+        .app{
+                padding: 10px;
+                background-color: #f5f5f5;
+                box-shadow: 0 0 10px rgba(0,0,0,0.1);
+                border-radius: 10px;
+
+        }
+        .person{
+                background-color: skyblue;
+                box-shadow: 0 0 10px rgba(0,0,0,0.1);
+                padding: 10px;
+
+        }
+        button{
+                margin: 0 5px;
+        }
+</style>
