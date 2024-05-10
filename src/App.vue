9f43897bfe7eccd4b41bcf4cd731d5e75b89d546
simon Li <13132549866@163.com>
2024-05-10 22:25:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+        <!-- html -->
+        <div class="app">
+                <h1>你好啊</h1>
+
+        </div>
+</template>
+
+<script lang="ts">
+// JS 或TS
+export default {
+        // 组件名
+                name:"App"
+        }
+</script>
+
+<style>
+        /* css */
+        .app{
+                padding: 10px;
+                background-color: #f5f5f5;
+                box-shadow: 0 0 10px rgba(0,0,0,0.1);
+
+        }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+// 引入App根组件
+import App from './App.vue'
+
+createApp(App).mount('#app')
