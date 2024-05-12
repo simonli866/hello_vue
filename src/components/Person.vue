8f756a736e7c12c2e35f559522100ddd3883c616
simon Li <13132549866@163.com>
2024-05-12 22:35:20 +0800

    ref 1/3

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,71 +1,63 @@
 <template>
         <!-- html -->
         <div class="person">
-                <h2>姓名：{{name}}</h2>
-                <h2>年龄：{{age}}</h2>
+                <h2>姓名：{{ name }}</h2>
+                <h2>年龄：{{ age }}</h2>
+                <h2>地址：{{ address }}</h2>
                 <button @click="changeName">修改名字</button>
                 <button @click="changeAge">修改年龄</button>
                 <button @click="showTel">查看联系方式</button>
         </div>
 </template>
 
-<script lang="ts">
+<!-- <script lang="ts">
 // JS 或TS
 export default {
         // 组件名
         name: "Person",
-        beforeCreate() {
-                console.log("Person组件创建前");
-        },
-        setup() {
-                console.log("Person组件创建后");
-                // 数据
-                // vue3中弱化了this，此时name，age，tel都不是响应式的
-                let name = "张三" 
-                // 此时的那么不是响应式的
-                let age = 18
-                let tel = "13333333"
-                // 方法
-                function changeName() {
-                        name = "李四"
-                }
-                function changeAge() {
-                        age +=1
-                }
-                function showTel() {
-                        alert(tel)
-                }
-                // return {
-                //         name,
-                //         age,
-                //         tel,
-                //         changeName,
-                //         changeAge,
-                //         showTel
-                // }
+};
+</script> -->
+<script lang="ts" setup>
+        import { ref } from "vue";
+        let name = ref("张三");
+        // 此时的那么不是响应式的
+        let age = ref(18);
+        let tel = "13333333";
+        let address = "杭州";
+        console.log(name);
+console.log(age);
+console.log(address);
+        console.log(tel);
+// 方法
+function changeName() {
+        name.value = "李四";
+}
 
-                // setup的返回值也可以是一个渲染函数
-                return  ()=> "haha"
-        }
+function changeAge() {
+        age.value += 1;
+}
+
+function showTel() {
+        alert(tel);
 }
 </script>
 
 <style scoped>
-        /* css */
-        .app{
-                padding: 10px;
-                background-color: #f5f5f5;
-                box-shadow: 0 0 10px rgba(0,0,0,0.1);
-                border-radius: 10px;
+/* css */
+.app {
+        padding: 10px;
+        background-color: #f5f5f5;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        border-radius: 10px;
+}
 
-        }
-        .person{
-                background-color: skyblue;
-                box-shadow: 0 0 10px rgba(0,0,0,0.1);
-                padding: 10px;
+.person {
+        background-color: skyblue;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        padding: 10px;
+}
 
-        }
-        button{
-                margin: 0 5px;
-        }
+button {
+        margin: 0 5px;
+}
 </style>
