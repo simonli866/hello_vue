d32897cd40f6e79c85b2958de4c9654d2f36153e
simon Li <13132549866@163.com>
2024-05-14 23:11:37 +0800

    watch change object

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,9 +1,12 @@
 <template>
         <!-- html -->
         <div class="person">
-                <h2>情况1:监视基本类型</h2>
-                <h2>当前求和为：{{ sum }}</h2>
-                <button @click="changeSum">sum+1</button>
+                <h2>情况2:监视对象类型</h2>
+                <h2>姓名:{{ person.name }}</h2>
+                <h2>年龄：{{ person.age }}</h2>
+                <button @click="changeName">修改姓名</button>
+                <button @click="changeAge">修改年龄</button>
+                <button @click="changePerson">修改对象</button>
         </div>
 </template>
 
@@ -16,20 +19,41 @@ export default {
 </script> -->
 <script lang="ts" setup name="Person">
 import { watch, ref } from "vue";
-
-let sum = ref(0);
-function changeSum() {
-        sum.value++;
+let person = ref({
+        name: "张三",
+        age: 18,
+        sex: "男",
+        address: "北京",
+});
+function changeAge() {
+        person.value.age += 1;
+}
+function changeName() {
+        person.value.name += "~";
 }
 
-const stopWatch = watch(sum, (newValue, oldValue) => {
-        console.log("sum变化了", newValue, oldValue);
-        if (newValue > 10) {
-                stopWatch();
-        }
-})
 
-console.log(stopWatch)
+function changePerson() {
+        person.value = {
+                name: "李四",
+                age: 20,
+                sex: "女",
+                address: "上海",
+        };
+}
+
+// 监视，情况一：监视ref定义的对象类型数据，监视的是对象的地址值，若想监视对象内部的属性，需要使用deep:true
+// watch(person, (newValue, oldValue) => {
+//         console.log("newValue", newValue);
+//         console.log("oldValue", oldValue);
+// }, {deep:true})
+// watch 的第三个参数是配置对象
+// watch 的第二个参数是监视的回调
+// watch 的第一个参数是监视的对象
+watch(person, (newValue, oldValue) => {
+        console.log("newValue", newValue);
+        console.log("oldValue", oldValue);
+}, { deep: true, immediate: true })
 </script>
 
 <style scoped>
